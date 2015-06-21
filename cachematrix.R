a3247c45b2f1aab542c57ebf06829feb678552b4
tsacchet@bates.edu <your_email@example.com>
2015-06-21 16:43:02 -0400

    Matrix Cache Function
    This is the edited matrix function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+MakeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  #Set the value of the matrix
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  #Get the value of the matrix
+  get<-function() x
+  Set.Matrix = function(solve) m<<- solve
+  Get.Matrix = function() m
+  list(set = set, get = get,
+       Set.Matrix = Set.Matrix,
+       Get.Matrix = Get.Matrix)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+  m = x$Get.Matrix()
+  #Set the value of the inverse
+  if(!is.null(m)){
+    message("Retrieving you Cached Data")
+    return(m)
+  }
+  #Get the value for the inverse
+  matrix = x$get()
+  m = solve(matrix, ...)
+  x$Set.Matrix(m)
+  m
 }
